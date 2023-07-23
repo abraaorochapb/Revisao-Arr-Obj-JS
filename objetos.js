d608a4d22a4629adecd7f272ed06767fdc6156e4
abraaorochapb <abraaorochapb@gmail.com>
2023-07-23 16:20:16 -0300

    Revisao objetos

---
+++ objetos.js
@@ -0,0 +1,14 @@
+// Em JS temos um tipo de dado que é o objeto, mas seu nome técnico é 'Object literals'.
+// Isso porque o Objeto vem da Orientação a Objetos, com outros recursos: instância, herança...
+// Já o literals possui apenas propriedades e metodos
+// O objeto fica em um bloco de {}
+
+const person = {
+  name: 'Ana',
+  age: 20,
+  favoriteFood: 'Pizza',
+  weight : 50
+}
+console.log(typeof person)
+console.log(person.age)
+console.log(person.name)
